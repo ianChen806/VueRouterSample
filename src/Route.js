222ec856ee85775cd63d87cc18b258944b846995
IanChen <ian.chen@taiwantaxi.com.tw>
2020-05-13 11:19:22 +0800

    introduce Route.js.

---
+++ src/Route.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from "vue-router";
+import HelloWorld from "./components/HelloWorld";
+import About from "./components/About";
+import Detail from "./components/Detail";
+import AboutUs from "./components/AboutUs";
+import AboutYou from "./components/AboutYou";
+
+Vue.use(VueRouter);
+
+export default new Vue({
+    router: new VueRouter({
+        routes: [
+            {path: "", component: HelloWorld},
+            {
+                path: "/About", component: About, children: [
+                    {path: "Us", component: AboutUs},
+                    {path: "You", component: AboutYou},
+                ]
+            },
+            {path: "/Detail/:id?", component: Detail},
+        ]
+    }),
+    render: h => h(App),
+}).$mount('#app')
+
--- src/main.js
+++ src/main.js
@@ -1,27 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from "vue-router";
-import HelloWorld from "./components/HelloWorld";
-import About from "./components/About";
-import Detail from "./components/Detail";
-import AboutUs from "./components/AboutUs";
-import AboutYou from "./components/AboutYou";
+import router from "./Route";
 
-Vue.use(VueRouter);
 Vue.config.productionTip = false
 
 new Vue({
-    router: new VueRouter({
-        routes: [
-            {path: "", component: HelloWorld},
-            {
-                path: "/About", component: About, children: [
-                    {path: "Us", component: AboutUs},
-                    {path: "You", component: AboutYou},
-                ]
-            },
-            {path: "/Detail/:id?", component: Detail},
-        ]
-    }),
+    router: router,
     render: h => h(App),
 }).$mount('#app')
